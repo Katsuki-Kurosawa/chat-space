4faa81de9368a813413138e256ffb9440c71bdd1
Katsuki-Kurosawa <kurosawakatsuki@kurosawakatsunorinoMacBook-Pro.local>
2017-06-09 13:51:38 +0900

    deviseの導入、モデルの作成、サインアップの実装 ユーザー情報編集機能

--- Gemfile
+++ Gemfile
@@ -48,10 +48,13 @@ group :development do
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
   gem "erb2haml"
+  gem 'pry-rails'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'haml-rails'
 gem 'font-awesome-rails'
+gem 'devise'
+
 
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+
+
+def edit
+  @user = User.find(current_user.id)
+end
+
+
+  def update
+
+@user = User.find(current_user.id)
+@user.update_attributes(user_params)
+redirect_to controller: :messages, action: :index
+
+  end
+
+
+def user_params
+params.require(:user).permit(:name, :email)
+end
+
+end
+
