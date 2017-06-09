b39b082426f1f20d6346bc1403c42b1e7c498a25
Katsuki-Kurosawa <kurosawakatsuki@kurosawakatsunorinoMacBook-Pro.local>
2017-06-09 14:25:53 +0900

    deviseの導入、モデルの作成、サインアップの実装 ユーザー情報編集機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,23 @@
 class UsersController < ApplicationController
 
 
-def edit
-  @user = User.find(current_user.id)
-end
+  def edit
+    @user = User.find(current_user.id)
+  end
 
 
   def update
 
-@user = User.find(current_user.id)
-@user.update_attributes(user_params)
-redirect_to controller: :messages, action: :index
+    @user = User.find(current_user.id)
+    @user.update_attributes(user_params)
+    redirect_to controller: :messages, action: :index
 
   end
 
 
-def user_params
-params.require(:user).permit(:name, :email)
-end
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 
 end
 
