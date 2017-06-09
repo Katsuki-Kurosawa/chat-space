a19536797f452e11008164182917483274c97380
Katsuki-Kurosawa <kurosawakatsuki@kurosawakatsunorinoMacBook-Pro.local>
2017-06-09 17:14:33 +0900

    ユーザー情報編集機能 修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,9 +7,8 @@ class ApplicationController < ActionController::Base
   protected
 
  def configure_permitted_parameters
-    added_attrs =[:name]
-    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
-    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
-    devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
+    devise_parameter_sanitizer.permit :sign_up, keys: [:name]
+    devise_parameter_sanitizer.permit :account_update, keys: [:name]
+    devise_parameter_sanitizer.permit :sign_in, keys: [:name]
 end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,11 +9,14 @@ class UsersController < ApplicationController
   def update
 
     @user = User.find(current_user.id)
-    @user.update_attributes(user_params)
-    redirect_to controller: :messages, action: :index
 
+    if current_user.id == @user.id
+      @user.update_attributes(user_params)
+      redirect_to root_path
+    else
+      redirect_to edit_user_path
+    end
   end
-
 
   def user_params
     params.require(:user).permit(:name, :email)
