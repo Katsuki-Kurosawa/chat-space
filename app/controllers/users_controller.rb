840509be2fbb5c8cedf524bf71694a374835b676
Katsuki-Kurosawa <kurosawakatsuki@kurosawakatsunorinoMacBook-Pro.local>
2017-06-12 20:24:58 +0900

    ユーザー情報編集機能 修正
    index.html.hamlの修正
    users_controller.rbの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,19 @@
 class UsersController < ApplicationController
- before_action :user_set
 
- def edit
- end
+  def edit
+  end
 
- def update
-   if current_user.id == @user.id
-    @user.update_attributes(user_params)
-    redirect_to root_path
-  else
-    render :user_set
+  def update
+    if current_user.update(user_params)
+     redirect_to root_path
+   else
+    render :edit
   end
 end
 
 private
 def user_params
   params.require(:user).permit(:name, :email)
-end
-
-def user_set
-  @user = User.find(current_user)
 end
 
 end
