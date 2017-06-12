49f907600c21407bef74a2b8045b3e57f4f83f5b
Katsuki-Kurosawa <kurosawakatsuki@kurosawakatsunorinoMacBook-Pro.local>
2017-06-12 17:00:44 +0900

    ユーザー情報編集機能 修正
    users_controller.rb　：　アクションの追加、修正しました
    _color.scss :　変数名の修正しました
    application.scss とindex.html.haml　も修正しましたのですみませんが確認をお願い致します。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,28 @@
 class UsersController < ApplicationController
+ before_action :edit
 
-
-  def edit
-    @user = User.find(current_user)
-  end
-
-
-  def update
-
-    @user = User.find(current_user)
-
-    if current_user.id == @user.id
-      @user.update_attributes(user_params)
-      redirect_to root_path
-    else
-      render :edit
-    end
-  end
-
-  def user_params
-    params.require(:user).permit(:name, :email)
-  end
-
+ def edit
+  user_update
 end
 
+
+def update
+ user_update
+ if current_user.id == @user.id
+  @user.update_attributes(user_params)
+  redirect_to root_path
+else
+  render :edit
+end
+end
+
+
+private
+def user_params
+  params.require(:user).permit(:name, :email)
+end
+end
+
+def user_update
+  @user = User.find(current_user)
+end
