d28e708bec5c49e8d610b9651aad9021f0903c8d
Katsuki-Kurosawa <kurosawakatsuki@kurosawakatsunorinoMacBook-Pro.local>
2017-06-14 12:47:35 +0900

    グループ機能実装
    groupコントローラ、group、group_user（中間テーブル）モデルの作成
    ルーティングの設定
    groupコントローラの定義（大枠）
    user、group、group_userモデルのアソシエーションの設定

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,15 @@
+class GroupsController < ApplicationController
+
+  def  new   #グループ、メンバーの作成
+  end
+
+  def create   #グループ、メンバーの保存
+  end
+
+  def edit   #グループ、メンバーの修正
+  end
+
+  def update   #グループ、メンバーの修正の更新
+  end
+
+end
