4e668f3ffc8aab6576d8062c9930e849de406fdf
Katsuki-Kurosawa <kurosawakatsuki@kurosawakatsunorinoMacBook-Pro.local>
2017-06-17 13:32:27 +0900

    グループ作成機能
    ・new.html.erbの修正（新規グループ作成時に使用のため）
    ・edit.html.erbの修正（グループ修正時に使用のため）
    ・グループ作成成功時のフラッシュメッセージの設定
    ・グループ保存失敗時のバリデーション、エラーメッセージの設定
    ・groups_controller.rbのアクションの設定

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,15 +1,31 @@
 class GroupsController < ApplicationController
 
   def  new   #グループ、メンバーの作成
+    @group = Group.new
   end
 
   def create   #グループ、メンバーの保存
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: "グループを作成しました"
+    else render :new
+    end
   end
 
   def edit   #グループ、メンバーの修正
+    @group = Group.find(params[:id])
   end
 
   def update   #グループ、メンバーの修正の更新
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to root_path
+    else render :edit
+    end
   end
+
+  def group_params
+   params.require(:group).permit(:name, :user_ids =>[])
+ end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170614032801) do
+ActiveRecord::Schema.define(version: 20170614074604) do
 
   create_table "group_users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer  "user_id",    null: false
-    t.integer  "group_id",   null: false
+    t.integer  "user_id"
+    t.integer  "group_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["group_id"], name: "index_group_users_on_group_id", using: :btree
@@ -46,4 +46,6 @@ ActiveRecord::Schema.define(version: 20170614032801) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "group_users", "groups"
+  add_foreign_key "group_users", "users"
 end
