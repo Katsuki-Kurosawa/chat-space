2b603dabc2de2d215b59c83f306de1a551274567
Katsuki-Kurosawa <kurosawakatsuki@kurosawakatsunorinoMacBook-Pro.local>
2017-06-20 13:22:31 +0900

    メッセージ送信機能の実装（修正）
    ・messages_controller.rbの createアクションの修正、groups_setアクションの定義

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,24 +1,30 @@
 class MessagesController < ApplicationController
 
+ before_action :groups_set
+
   def index    #ホーム画面
-    @groups = current_user.groups
-    @group= Group.find(params[:group_id])
     @message = Message.new
   end
 
   def create   #メッセージ送信、保存
-    @message = Message.create(body:message_params[:body], image:message_params[:image], group_id:params[:group_id], user_id: current_user.id)
-    if @message.save
-      redirect_to group_messages_path, notice: "メッセージを送信しました"
-    else
-      flash[:alert] = "メッセージを入力してください"
-      redirect_to group_messages_path
-    end
+   @message = Message.new(message_params)
+   if @message.save
+    redirect_to group_messages_path, notice: "メッセージを送信しました"
+  else
+    flash[:alert] = "メッセージを入力してください"
+    render :index
   end
+end
 
-  private
-  def message_params
-    params.require(:message).permit(:body, :image)
-  end
+private
+def message_params
+  params.require(:message).permit(:body, :image).merge(user_id: current_user.id,
+    group_id: params[:group_id])
+end
+
+def groups_set
+  @groups = current_user.groups
+  @group= Group.find(params[:group_id])
+end
 
 end
