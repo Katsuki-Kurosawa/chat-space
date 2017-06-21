c745c46dd16816ac6d2ccedbb7940ae68183e034
Katsuki-Kurosawa <kurosawakatsuki@kurosawakatsunorinoMacBook-Pro.local>
2017-06-21 20:29:14 +0900

    メッセージ送信機能を実装（修正）
    ・messages_controller.rb　のメソッドの修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,13 +1,14 @@
 class MessagesController < ApplicationController
 
-  before_action :groups_set, only: [:index, :create]
-  before_action :set_new_message, only: [:create]
+  before_action :set_groups, only: [:index, :create]
+  before_action :set_group_messages, only: [:index, :create]
 
   def index    #ホーム画面
    @message = Message.new
  end
 
   def create   #メッセージ送信、保存
+   @message = Message.new(message_params)
    if @message.save
     redirect_to group_messages_path, notice: "メッセージを送信しました"
   else
@@ -23,13 +24,12 @@ def message_params
     group_id: params[:group_id])
 end
 
-def set_new_message
-  @message = Message.new(message_params)
+def set_groups
+  @groups = current_user.groups
 end
 
-def groups_set
-  @groups = current_user.groups
-  @group= Group.find(params[:group_id])
+def set_group_messages
+  @group = Group.find(params[:group_id])
   @messages = @group.messages.includes(:user)
 end
 
