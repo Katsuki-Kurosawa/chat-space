5be82008668a69f4e4f2fd9ed0b84a6b7d6752e9
Katsuki-Kurosawa <kurosawakatsuki@kurosawakatsunorinoMacBook-Pro.local>
2017-06-23 13:45:13 +0900

    投稿機能のテスト（モデル編）（修正）
    ・actories/messages.rb　（bodyカラムのfakerによるランダムの文字列生成の設定）
    ・message_spec.rb（contextの設定）

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
 
   factory :message do
-    body              "hello"
+    body              Faker::Base.regexify("[a-z]{10}")
     image             Rack::Test::UploadedFile.new Rails.root.join('public/uploads/twitter-icon-circle-logo-400x400.jpg')
     group_id          "3"
     user_id           "2"
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -2,38 +2,42 @@ require 'rails_helper'
 describe Message do
   describe '#create' do
 
-    it "メッセージがあれば保存できる" do
-      message =  build(:message, image:nil)
-      expect(message).to be_valid
+    context  '・メッセージを保存できる場合 ' do
+      it "メッセージがあれば保存できる" do
+        message =  build(:message, image:nil)
+        expect(message).to be_valid
+      end
+
+      it "画像があれば保存できる" do
+        message =  build(:message, body:nil)
+        expect(message).to be_valid
+      end
+
+      it "メッセージと画像があれば保存できる" do
+        message =  build(:message)
+        expect(message).to be_valid
+      end
     end
 
-    it "画像があれば保存できる" do
-      message =  build(:message, body:nil)
-      expect(message).to be_valid
-    end
+    context  '・メッセージを保存できない場合 ' do
+      it "メッセージも画像も無いと保存できない" do
+        message =  build(:message,body:nil,image:nil)
+        message.valid?
+        expect(message.errors[:body]).to include("translation missing: ja.activerecord.errors.models.message.attributes.body.blank")
+        expect(message.errors[:image]).to include("translation missing: ja.activerecord.errors.models.message.attributes.image.blank")
+      end
 
-    it "メッセージと画像があれば保存できる" do
-      message =  build(:message)
-      expect(message).to be_valid
-    end
+      it "group_idが無いと保存できない" do
+        message =  build(:message,group_id:nil)
+        message.valid?
+        expect(message.errors[:group_id]).to include("translation missing: ja.activerecord.errors.models.message.attributes.group_id.blank")
+      end
 
-    it "メッセージも画像も無いと保存できない" do
-      message =  build(:message,body:nil,image:nil)
-      message.valid?
-      expect(message.errors[:body]).to include("translation missing: ja.activerecord.errors.models.message.attributes.body.blank")
-      expect(message.errors[:image]).to include("translation missing: ja.activerecord.errors.models.message.attributes.image.blank")
-    end
-
-    it "group_idが無いと保存できない" do
-      message =  build(:message,group_id:nil)
-      message.valid?
-      expect(message.errors[:group_id]).to include("translation missing: ja.activerecord.errors.models.message.attributes.group_id.blank")
-    end
-
-    it "user_idが無いと保存できない" do
-      message =  build(:message,user_id:nil)
-      message.valid?
-      expect(message.errors[:user_id]).to include("translation missing: ja.activerecord.errors.models.message.attributes.user_id.blank")
+      it "user_idが無いと保存できない" do
+        message =  build(:message,user_id:nil)
+        message.valid?
+        expect(message.errors[:user_id]).to include("translation missing: ja.activerecord.errors.models.message.attributes.user_id.blank")
+      end
     end
   end
 end
