8dcb5be8df9c862a89fa6f1fbb5cb3a5cc35eeb1
Katsuki-Kurosawa <kurosawakatsuki@kurosawakatsunorinoMacBook-Pro.local>
2017-06-26 15:33:07 +0900

    投稿機能のテスト（モデル編）（修正）
    インデントの修正

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,44 +1,43 @@
 require 'rails_helper'
-describe Message do
-  describe '#create' do
+  describe Message do
+    describe '#create' do
 
-   context  '・メッセージを保存できる場合 ' do
-     let(:messages){ FactoryGirl.build(:message) }
+       context  '・メッセージを保存できる場合 ' do
+         let(:messages){ FactoryGirl.build(:message) }
 
-     it "メッセージがあれば保存できる" do
-       messages[:image] = nil
-       expect(messages).to be_valid
-     end
+         it "メッセージがあれば保存できる" do
+           messages[:image] = nil
+           expect(messages).to be_valid
+         end
 
-     it "画像があれば保存できる" do
-       messages[:body] = nil
-       expect(messages).to be_valid
-     end
+         it "画像があれば保存できる" do
+           messages[:body] = nil
+           expect(messages).to be_valid
+         end
 
-     it "メッセージと画像があれば保存できる" do
-      expect(messages).to be_valid
+         it "メッセージと画像があれば保存できる" do
+          expect(messages).to be_valid
+        end
+      end
+
+      context  '・メッセージを保存できない場合 ' do
+        let(:messages) { FactoryGirl.build(:message,body:nil,image:nil,group_id:nil,user_id:nil) }
+        it "メッセージも画像も無いと保存できない" do
+         messages.valid?
+         expect(messages.errors[:body]).to include("を入力してください")
+         expect(messages.errors[:image]).to include("を入力してください")
+       end
+
+       it "group_idが無いと保存できない" do
+        messages.valid?
+        expect(messages.errors[:group_id]).to include("を入力してください")
+      end
+
+      it "user_idが無いと保存できない" do
+       messages.valid?
+       expect(messages.errors[:user_id]).to include("を入力してください")
+      end
     end
   end
-
-  context  '・メッセージを保存できない場合 ' do
-    let(:messages) { FactoryGirl.build(:message,body:nil,image:nil,group_id:nil,user_id:nil) }
-    it "メッセージも画像も無いと保存できない" do
-     messages.valid?
-     expect(messages.errors[:body]).to include("を入力してください")
-     expect(messages.errors[:image]).to include("を入力してください")
-   end
-
-
-   it "group_idが無いと保存できない" do
-    messages.valid?
-    expect(messages.errors[:group_id]).to include("を入力してください")
-  end
-
-  it "user_idが無いと保存できない" do
-   messages.valid?
-   expect(messages.errors[:user_id]).to include("を入力してください")
- end
-end
-end
 end
 
