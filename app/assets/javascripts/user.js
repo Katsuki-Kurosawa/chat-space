d7e2b094951f596bd96e411f9497e109c104b074
Katsuki-Kurosawa <kurosawakatsuki@kurosawakatsunorinoMacBook-Pro.local>
2017-07-05 16:44:16 +0900

    ユーザー検索のインクリメンタルサーチ化の設定(修正)
    ・user.js（HTMLの構造の記述の修正する処理）
    ・_group.scss（カラーを変数に修正する処理）
    ・_color.scss（カラーを変数にする処理）

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,19 +1,27 @@
 $(function() {
 
   function  appendUser(user) {
-    var html =`
-  <div class="chat-group-users clearfix">
-  <p class="chat-group-user__name">${user.name}</p>
-  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${user.id} data-user-name=${user.name}>追加</a></div>`
+    var html =
+      '<div class="chat-group-users clearfix">' +
+      '<p class="chat-group-user__name">' +
+      user.name +
+      '</p>' +
+      '<a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="' + user.id + '" data-user-name="' + user.name + '">追加' +
+      '</a>' +
+      '</div>';
     $("#user-search-result").append(html);
   }
 
   function buildUser(id, name) {
-    var html =`
-    <div class="chat-group-user clearfix" id="chat-group-user-${id}">
-    <input type="hidden" name="group[user_ids][]" value="${id}">
-    <p class="chat-group-user__name">${name}</p>
-    <a class="user-search-remove chat-group-user__btn chat-group-user__btn--remove" data-user-id="${id}">削除</a></div>`
+    var html =
+      '<div class="chat-group-user clearfix" id="chat-group-user-' + id +  '">' +
+      '<input type="hidden" name="group[user_ids][]" value="' + id + '">' +
+      '<p class="chat-group-user__name">' +
+      name +
+      '</p>' +
+      '<a class="user-search-remove chat-group-user__btn chat-group-user__btn--remove" data-user-id="' + id + '">削除' +
+      '</a>' +
+      '</div>';
    $('#chat-group-users').append(html);
    $('.chat-group-users').remove();
    $this.parent('.chat-group-user').remove();
